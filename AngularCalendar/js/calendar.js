c0824742ba9492a4d736b687ae6200b2c6c466b5
Trent Nielson <trent@custombit.com>
2016-04-15 08:50:30 -0600

    Adds Calendar Title Bar
    - Adds additional get scopes
    - remove irrelevant content
    - problem with alignment of Titlebar header

--- AngularCalendar/js/calendar.js
+++ AngularCalendar/js/calendar.js
@@ -1,12 +1,18 @@
 var testapp = angular.module('testapp', []);
 
 testapp.controller('ctrl1', function($scope){
+  $scope.months=["January", "February", "March", "April", "May", "June", "July", "August", "July", "September", "October", "November", "December" ];
+  $scope.weekdays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   $scope.date = new Date;
   $scope.Month = $scope.date.getMonth();
-  $scope.Day = $scope.date.getDay();
+  $scope.WeekDay = $scope.date.getDay();
+  $scope.Year = $scope.date.getFullYear();
+  $scope.DayOfMonth = $scope.date.getDate();
+  $scope.CurrMonth = $scope.months[$scope.Month];
 });
 
-testapp.controller('badCtrl', function($scope) {
-  var badFeelings = ["Disregarded", "Unimportant","Rejected","Powerless"];
-  $scope.bad = badFeelings[Math.floor((Math.random() * 4))];
+testapp.controller('goodCtrl', function($scope) {
+  var goodFeelings = ["Ready", "Awesome","Inner Peace"];
+  $scope.good = goodFeelings[Math.floor((Math.random() * 3))];
+
 });
