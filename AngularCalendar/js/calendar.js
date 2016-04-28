2bb10497b727c380d43018fc32818d1f5a2e5276
Trent Nielson <trent@custombit.com>
2016-04-27 19:32:45 -0600

    Reformatting arrays & updating variable references.  https://github.com/tniels22/JS/issues/1
    -Renamed App
    -Added temp notes
    -Set days in month not working. Needs to be moved to Service? Won't
    update array.

--- AngularCalendar/js/calendar.js
+++ AngularCalendar/js/calendar.js
@@ -1,30 +1,78 @@
-var testapp = angular.module('testapp', []);
+var testapp = angular.module('app', []);
 
-testapp.controller('SetVariables', function(){
+testapp.controller('SetVariables', [function(){
   //initialized / construction
-  var months=["January", "February", "March", "April", "May", "June", "July", "August", "July", "September", "October", "November", "December" ];
-  var DaysInMonth=[31, 28, 31, 30, 31, 31, 30, 31, 30, 31]
   var self = this;
+  self.months = [
+    {month:"January", days: 31},
+    {month: "February", days: 28},
+    {month: "March", days: 31},
+    {month: "April", days: 30},
+    {month: "May", days: 31},
+    {month: "June", days: 30},
+    {month: "July", days: 31},
+    {month: "August", days: 31},
+    {month: "September", days: 30},
+    {month: "October", days: 31},
+    {month: "November", days: 30},
+    {month: "December", days: 31}
+   ];
+
+  self.weekdays = [
+    {day: "Sunday"},
+    {day: "Monday"},
+    {day: "Tuesday"},
+    {day: "Wednesday"},
+    {day: "Thursday"},
+    {day: "Friday"},
+    {day: "Saturday"}
+  ];
+
   self.date = new Date;
+
+  // Use these in a service....
   self.Month = self.date.getMonth();
   self.WeekDay = self.date.getDay();
   self.Year = self.date.getFullYear();
   self.DayOfMonth = self.date.getDate();
-  // create functions
-  self.NameOfMonth = months[self.Month];
-  self.GetDaysInMonth = DaysInMonth[self.Month];
-  self.GetDaysInPrevMonth = DaysInMonth[self.Month-1];
-  self.GetDaysInNextMonth = DaysInMonth[self.Month+1];
+  self.NameOfMonth = self.months[self.Month].month;
+  self.NumOfDaysInMonth = self.months[self.Month].days;
+  self.DaysInMonth = [];
+  //returns arrray with each day of Month
+  self.SetDaysInMonth = function(){
+    var a=[];
+    for(var i=0; i < self.DaysInMonth[self.Month]; i++) {
+      i++;
+      self.DaysInMonth.push(i);
+    }
+  }
+}]);
+testapp.controller('goodCtrl', function(self) {
+  var goodFeelings = ["Ready", "Awesome","Inner Peace"];
+  self.good = goodFeelings[Math.floor((Math.random() * 3))];
 });
-/*
+
+/* MY RANDOM NOTES
 testapp.controller('createCalendar', function(){
   self.GetDaysInMonth.foreach{
     angular.element(document.querySelector('#Calendar'));
   };
 });
+Imports angular
+Calendarapp.js
+Calendarcontroller.js
+Eventscontroller.js
+
+Eventservice.js
+
+Use ng-class to evaluate whats in the controller to determine if month is gray or black
+Julian numbers are easier…
+Javascript splice function
+
+SetMonth(0 based)
+Day of Month is (1 based)
+Track by $index
+
+
 */
 //Use Table
-testapp.controller('goodCtrl', function(self) {
-  var goodFeelings = ["Ready", "Awesome","Inner Peace"];
-  self.good = goodFeelings[Math.floor((Math.random() * 3))];
-});
